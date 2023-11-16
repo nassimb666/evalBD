0c04f59fe6a465056281076fea49f175437c1547
nassimb666 <bnassim750@gmail.com>
2023-11-16 11:09:34 +0100

    bouton affichage auteurs + card mais bug sur l'affichages des series

--- js/modeleBD.js
+++ js/modeleBD.js
@@ -14,6 +14,76 @@ document.addEventListener('DOMContentLoaded', function () {
     var imgAlbumMini = document.getElementById("albumMini");
     var id = document.getElementById("id");
 
+    var tableBody = document.getElementById('tableBody');
+    var showAllButton = document.getElementById('showAllButton');
+    var isListeVisible = false;
+    var searchAuthorInput = document.getElementById('searchAuthor');
+    var searchButton = document.getElementById('searchButton');
+    var authorWorksContainer = document.getElementById('authorWorks');
+
+    searchButton.addEventListener('click', function () {
+        var authorName = searchAuthorInput.value.trim().toLowerCase();
+
+        // Filtrer les œuvres de l'auteur
+        var authorWorks = Array.from(albums.values()).filter(function (album) {
+            var author = auteurs.get(album.idAuteur);
+            return author && author.nom.toLowerCase() === authorName;
+        });
+
+        // Afficher les œuvres de l'auteur dans des cards
+        displayAuthorWorks(authorWorks);
+    });
+
+    function displayAuthorWorks(works) {
+        // Effacer le contenu actuel
+        authorWorksContainer.innerHTML = "";
+
+        // Afficher les œuvres dans des cards
+        works.forEach(function (work) {
+            var card = createWorkCard(work);
+            authorWorksContainer.appendChild(card);
+        });
+    }
+
+    function createWorkCard(work) {
+        var card = document.createElement('div');
+        card.className = 'card';
+    
+        var cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+    
+        var title = document.createElement('h5');
+        title.className = 'card-title';
+        title.textContent = work.titre;
+    
+        var seriesElement = document.createElement('p');
+        seriesElement.className = 'card-text';
+    
+        // Recherche de la série correspondante dans la Map des séries
+        var serie = albums.get(work.idSerie);
+    
+        // Vérifie si la série existe avant d'accéder à ses propriétés
+        seriesElement.textContent = serie ? "Série : " + serie.nom : "Série : Inconnue";
+    
+        var number = document.createElement('p');
+        number.className = 'card-text';
+        number.textContent = "Numéro : " + work.numero;
+    
+        var price = document.createElement('p');
+        price.className = 'card-text';
+        price.textContent = "Prix : " + work.prix + " €";
+    
+        cardBody.appendChild(title);
+        cardBody.appendChild(seriesElement);
+        cardBody.appendChild(number);
+        cardBody.appendChild(price);
+    
+        card.appendChild(cardBody);
+    
+        return card;
+    }
+    
+
     imgAlbum.addEventListener("error", function () {
         prbImg(this);
     });
@@ -24,6 +94,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     id.addEventListener("change", function () {
         getAlbum(this);
+    });
+
+    showAllButton.addEventListener('click', function () {
+        if (isListeVisible) {
+            // Si la liste est déjà visible, on la cache
+            tableBody.innerHTML = "";
+            isListeVisible = false;
+        } else {
+            // Sinon, on l'affiche
+            afficherTousLesAuteurs();
+            isListeVisible = true;
+        }
     });
 
     function getAlbum(num) {
@@ -41,10 +123,10 @@ document.addEventListener('DOMContentLoaded', function () {
             var serie = series.get(album.idSerie);
             var auteur = auteurs.get(album.idAuteur);
 
-            txtSerie.value = serie.nom;
+            txtSerie.value = serie ? serie.nom : "";
             txtNumero.value = album.numero;
             txtTitre.value = album.titre;
-            txtAuteur.value = auteur.nom;
+            txtAuteur.value = auteur ? auteur.nom : "";
             txtPrix.value = album.prix;
 
             var nomFic = serie.nom + "-" + album.numero + "-" + album.titre;
@@ -79,9 +161,11 @@ document.addEventListener('DOMContentLoaded', function () {
         btn.addEventListener('click', function () {
             if (this.id == 'all') {
                 afficherTousLesAuteurs();
+                isListeVisible = true;
             } else {
                 var authorId = this.id;
                 cacherTousLesAuteursSaufUn(authorId);
+                isListeVisible = false;
             }
 
             btns.forEach(function (btn) {
@@ -92,42 +176,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    document.getElementById('showAllButton').addEventListener('click', function () {
-        afficherTousLesAuteurs();
-    });
-
     function afficherTousLesAuteurs() {
-        var parent = document.getElementById('parent');
-        var tableContainer = document.getElementById('tableContainer');
-        var tableBody = document.getElementById('tableBody');
-
-        // Supprimer les noms d'auteurs s'ils sont déjà affichés
-        parent.innerHTML = "";
+        // Supprimer les lignes existantes dans le tableau
         tableBody.innerHTML = "";
-        btns.forEach(function (btn) {
-            btn.classList.remove('active');
-        });
 
-        // Ajouter les noms d'auteurs à la liste et au tableau
+        // Ajouter les noms d'auteurs au tableau
         auteurs.forEach(function (auteur, idAuteur) {
-            var auteurDiv = document.createElement('div');
-            auteurDiv.id = idAuteur;
-            auteurDiv.className = 'auteur';
-            auteurDiv.textContent = auteur.nom;
-            parent.appendChild(auteurDiv);
-
-            // Ajouter une ligne au tableau
             var tableRow = document.createElement('tr');
             var tableData = document.createElement('td');
             tableData.textContent = auteur.nom;
             tableRow.appendChild(tableData);
             tableBody.appendChild(tableRow);
         });
-
-        document.getElementById('showAllButton').classList.add('active');
-
-        parent.style.display = "block";
-        tableContainer.style.display = "block";
     }
 
     function cacherTousLesAuteursSaufUn(authorId) {
